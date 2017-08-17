87ef61679ebe33516dcee529ea328826cb0c9163
Minho Menezes <minhotmog@gmail.com>
2017-08-17 00:01:39 -0300

    Start Jupyter Notebooks and separating Python3 raw code

--- raw_codes/regLinear.py
+++ raw_codes/regLinear.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
-    Title:      Logistic Regression
+    Title:      Linear Regression
     Author:     Minho Menezes
     
-    This is the code to perform a simple Logistic Regression on a simplified Iris Dataset.
+    This is the code to perform a simple Linear Regression on a artificial generated dataset.
 '''
 
 # Libraries
@@ -11,51 +11,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Functions
-def sigmoid(x):
-    ''' Returns the Sigmoid Function applied to a vector (or value) x '''
-    return 1 / (1 + np.exp(-1 * x))
-    
 def h_theta(x, theta):
     ''' Apply the Linear Model for features X and parameters theta '''
-    return sigmoid(np.dot(np.transpose(theta), x))
+    return np.dot(np.transpose(theta), x)
 
 def errorFunction(errors):
     ''' Calculate the Least Square Error '''
-    return (1 / np.size(errors)) * np.sum(errors ** 2)
-
-def modelAccuracy(x, y, theta):
-    ''' Calculates the percentage of correct classifications '''
-    return
-    
-def decisionBound(theta, x):
-    ''' Calculates and returns a linear Decision Boundary from the model '''
-    boundary_X = np.linspace(min(x[1,:]), max(x[1,:]))
-    boundary_Y = -1*(theta[1] / theta[2]) * boundary_X - (theta[0] / theta[2]);
-    return [boundary_X, boundary_Y]
-
-#  Main Function
+    return (1 / np.size(errors)) * np.sum(errors ** 2)   
+ 
+# Main Function
 if __name__=='__main__':
-   
+    
     ###############################
     # Part 1: Data Pre-Processing #
     ###############################
     # Loads the data
-    data = np.loadtxt("datasets/irisDataSimple.txt")
+    data = np.loadtxt("datasets/cricketData.txt")
     
     n_examples = np.size(data,0)
-    n_features = np.size(data,1) - 2
+    n_features = np.size(data,1)
     
     # Define the model parameters
-    x = np.array([np.ones(n_examples), data[:, 0], data[:, 1]])
-    y = data[:, 4]
+    x = np.array([np.ones(n_examples), data[:, 0]])
+    y = data[:, 1]
     theta = np.zeros([np.size(x, 0), 1])
     
     # Defines the hyperparameters and training measurements
-    alfa = 0.1
+    alfa = 0.005
     max_epochs = 500000
     
     error_hist = np.zeros([max_epochs])
-    epsilon = 0.001
+    epsilon = 0.01
     
     ######################################
     # Part 2: Linear Regression Training #
@@ -64,7 +50,7 @@ if __name__=='__main__':
         # Calculate the error vector from the current Model
         y_pred = h_theta(x, theta)
         error = y_pred - y
-        
+
         # Append new Least Square Error to History
         error_hist[epochs] = errorFunction(error)
 
@@ -72,8 +58,8 @@ if __name__=='__main__':
         for j in range(n_features):
             theta[j] = theta[j] - (alfa/n_examples) * np.sum(error * x[j,:])
 
-        # Prints training status at each 50 epochs    
-        if(epochs % 50 == 0):
+        # Prints training status at each 100 epochs
+        if(epochs % 500 == 0):
             print("###### Epoch", epochs, "######")
             print("Error:", error_hist[epochs])
             print("Thetas:\n", theta)
@@ -82,41 +68,39 @@ if __name__=='__main__':
         # Evaluate convergence and stops training if so
         if(abs(error_hist[epochs] - error_hist[epochs-50]) <= epsilon):
             print("Gradient Converged!!!\nStopping at epoch", epochs)
+            print("###### Epoch", epochs, "######")
+            print("Error:", error_hist[epochs])
+            print("Thetas:\n", theta)
+            print("")
             break
-    
+            
     #############################################
     # Part 3: Data Plotting and Training Result #
     #############################################
     # First Figure: Dataset plotting
     plt.figure(1)
     
-    plt.title("Simplified Iris Dataset Classification\n(Green=Iris-setosa; Blue=Iris-virginica)")
-    plt.xlabel("Sepal width (cm)")
-    plt.ylabel("Sepal length (cm)")
-    
-    pos = np.where(y == 1)
-    neg = np.where(y == 0)
+    plt.title("Influence of Temperature on Cricket Chirp Rate")
+    plt.xlabel("Rate of Cricket Chirping")
+    plt.ylabel("Temperature (ºF)")
     
     plt.grid()
-    plt.plot(x[1,pos], x[2,pos], 'go', x[1,neg], x[2,neg], 'bo')
+    plt.plot(x[1,:], y, 'rx')
     
     plt.show()
-
+    
     # Second Figure: Training results
     plt.figure(2)
-    deciBound = decisionBound(theta, x)
     
     plt.subplot(1,2,1)
-    
-    plt.title("Decision Boundary\n(Green=Iris-setosa; Blue=Iris-virginica; Black=DecisionBoundary)")
-    plt.xlabel("Sepal width (cm)")
-    plt.ylabel("Sepal length (cm)")
+    plt.title("Linear Regression Function Prediction\n(Black=ModelPrediction)")
+    plt.xlabel("Rate of Cricket Chirping")
+    plt.ylabel("Temperature (ºF)")
     
     plt.grid()
-    plt.plot(x[1,pos], x[2,pos], 'go', x[1,neg], x[2,neg], 'bo', deciBound[0], deciBound[1], 'k-')
+    plt.plot(x[1,:], y, 'rx', x[1,:], h_theta(x, theta)[0,:], 'k-')
     
     plt.subplot(1,2,2)
-    
     plt.title("Error History")
     plt.xlabel("Epochs")
     plt.ylabel("Least Square Error")
@@ -125,5 +109,5 @@ if __name__=='__main__':
     plt.plot(error_hist[:epochs], "g-")
     
     plt.show()
-
+    
 #__
---
+++ raw_codes/regLogistica.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+'''
+    Title:      Logistic Regression
+    Author:     Minho Menezes
+    
+    This is the code to perform a simple Logistic Regression on a simplified Iris Dataset.
+'''
+
+# Libraries
+import numpy as np 
+import matplotlib.pyplot as plt
+
+# Functions
+def sigmoid(x):
+    ''' Returns the Sigmoid Function applied to a vector (or value) x '''
+    return 1 / (1 + np.exp(-1 * x))
+    
+def h_theta(x, theta):
+    ''' Apply the Linear Model for features X and parameters theta '''
+    return sigmoid(np.dot(np.transpose(theta), x))
+
+def accuracyFunction(X, y, theta):
+    ''' Calculates the percentage of correct classifications '''
+    Y_pred = h_theta(X, theta)
+    pos = np.where(Y_pred >= 0.5); Y_pred[pos] = 1
+    neg = np.where(Y_pred < 0.5); Y_pred[neg] = 0
+    
+    return 100 * (1 - (1 / np.size(y)) * np.sum((Y_pred - y) ** 2))
+    
+def decisionBound(theta, x):
+    ''' Calculates and returns a linear Decision Boundary from the model '''
+    boundary_X = np.linspace(min(x[1,:]), max(x[1,:]))
+    boundary_Y = -1*(theta[1] / theta[2]) * boundary_X - (theta[0] / theta[2]);
+    return [boundary_X, boundary_Y]
+
+#  Main Function
+if __name__=='__main__':
+   
+    ###############################
+    # Part 1: Data Pre-Processing #
+    ###############################
+    # Loads the data
+    data = np.loadtxt("datasets/irisDataSimple.txt")
+    
+    n_examples = np.size(data,0)
+    n_features = np.size(data,1) - 2
+    
+    # Define the model parameters
+    x = np.array([np.ones(n_examples), data[:, 0], data[:, 1]])
+    y = data[:, 4]
+    theta = np.zeros([np.size(x, 0), 1])
+    
+    # Defines the hyperparameters and training measurements
+    alfa = 0.05
+    max_epochs = 500000
+    
+    error_hist = np.zeros([max_epochs])
+    epsilon = 0.01
+    
+    ######################################
+    # Part 2: Linear Regression Training #
+    ######################################
+    for epochs in range(max_epochs):
+        # Calculate the error vector from the current Model
+        y_pred = h_theta(x, theta)
+        error = y_pred - y
+        
+        # Append new Least Square Error to History
+        error_hist[epochs] = accuracyFunction(x, y, theta)
+
+        # Perform Gradient Descent
+        for j in range(n_features):
+            theta[j] = theta[j] - (alfa/n_examples) * np.sum(error * x[j,:])
+
+        # Prints training status at each 50 epochs    
+        if(epochs % 10 == 0):
+            print("###### Epoch", epochs, "######")
+            print("Error:", error_hist[epochs], "%")
+            print("Thetas:\n", theta)
+            print("")
+        
+        # Evaluate convergence and stops training if so
+        if(abs(error_hist[epochs] - error_hist[epochs-50]) <= epsilon):
+            print("Gradient Converged!!!\nStopping at epoch", epochs)
+            print("###### Epoch", epochs, "######")
+            print("Error:", error_hist[epochs], "%")
+            print("Thetas:\n", theta)
+            break
+    
+    #############################################
+    # Part 3: Data Plotting and Training Result #
+    #############################################
+    # First Figure: Dataset plotting
+    plt.figure(2)
+    
+    plt.title("Simplified Iris Dataset Classification\n(Green=Iris-setosa; Blue=Iris-virginica)")
+    plt.xlabel("Sepal width (cm)")
+    plt.ylabel("Sepal length (cm)")
+    
+    pos = np.where(y == 1)
+    neg = np.where(y == 0)
+    
+    plt.grid()
+    plt.plot(x[1,pos], x[2,pos], 'go', x[1,neg], x[2,neg], 'bo')
+    
+    plt.show()
+
+    # Second Figure: Training results
+    plt.figure(3)
+    deciBound = decisionBound(theta, x)
+    
+    plt.subplot(1,2,1)
+    
+    plt.title("Decision Boundary\n(Green=Iris-setosa; Blue=Iris-virginica; Black=DecisionBoundary)")
+    plt.xlabel("Sepal width (cm)")
+    plt.ylabel("Sepal length (cm)")
+    
+    plt.grid()
+    plt.plot(x[1,pos], x[2,pos], 'go', x[1,neg], x[2,neg], 'bo', deciBound[0], deciBound[1], 'k-')
+    
+    plt.subplot(1,2,2)
+    
+    plt.title("Error History")
+    plt.xlabel("Epochs")
+    plt.ylabel("Least Square Error")
+    
+    plt.grid()
+    plt.plot(error_hist[:epochs], "g-")
+    
+    plt.show()
+
+#__
